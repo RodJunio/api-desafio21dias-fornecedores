0f79cbaf3ad36a5fe534904d026cde5fa994062a
RodJunio <devjuniorodrigues@gmail.com>
2023-06-28 22:06:00 -0300

    Adicionar arquivos de projeto.

---
+++ Servico/DbContexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace api_desafio21dias.Servicos
+{
+    public class DbContexto : DbContext
+    {
+        public DbContexto(DbContextOptions<DbContexto> options) : base(options) { }
+       
+       
+    }
+}
